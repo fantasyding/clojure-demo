843b8625400fe141ebe81c8f6c417f04fa59a5c5
dingxin <dingxin@coincard.cc>
2017-11-24 17:15:44 +0800

    加入ring插件

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Just do it!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring "1.6.3"]
                  [compojure "1.6.0"]
@@ -13,6 +13,10 @@
                                               javax.jms/jms
                                               com.sun.jdmk/jmxtools
                                               com.sun.jmx/jmxri]]]
+  :plugins [[lein-ring "0.12.1"]]
+  :ring {:handler        demo.core/app
+         :auto-reload?  true
+         :auto-refresh? true}
   :main ^:skip-aot demo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
